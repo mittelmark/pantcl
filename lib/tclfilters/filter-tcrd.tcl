9348b389655e20bc56b159d5e27592da1481cb1c
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-07-15 23:34:30 +0200

    filter-tcrd chord diagrams with widht option

--- lib/tclfilters/filter-tcrd.tcl
+++ lib/tclfilters/filter-tcrd.tcl
@@ -18,7 +18,7 @@
 #' ## Name
 #' 
 #' _filter-tcrd.tcl_ - Pandoc Tcl filter which can be used to display songs with chords
-#'   with music chord names above the text where the chords are directly embedded within the song text.
+#'   with music chord names above the text where the chords are directly embedded within the song text. Currently only one chord per word can be displayed.
 #' 
 #' ## Usage
 #' 
@@ -35,23 +35,26 @@
 #' The following options can be given via code chunks options or as defaults in the YAML header.
 #' 
 #' > - eval - should the code in the code block be evaluated, default: false (0)
-#'   - chord - are hte code chunk contain chord commands like `C 0003` to display chord charts for fretted instruments, default: false
+#'   - circlecolor - for chords charts the circle color, default: back
+#'   - chord - are the code chunk contain chord commands like `C 0003` to display chord charts for fretted instruments, default: false
+#'   - ext  - the file extension if chord files are displayed, pdf or png are possible if cairosvg is installed, default: svg
 #'   - imagepath - if chord diagrams are generated the path for the stored images in svg format, default: images
 #'   - inline - are chords embedded inline within the text like this `Text [C]with [Am]chord!`, default: true (1)
 #'   - label - the code chunk label used as well for the image name, default: null
 #'   - results - should the output, the song text be show(n) or hid(d)e(n), default: show
 #'   - transpose - how many steps up or down to transpose, can be negative or positive, 0 means no transpose default: 0
+#'   - width - for chord charts the width of the chart, for Guitar a value of 160  is reasonable, for Ukulele a value of 100, default: 100
 #' 
-#' Options can be set globally in the YAML header for all chunks. Below is an example which shows only the source but not the output.
+#' Options can be set globally in the YAML header for all chunks. Below is an example which hides per default always the source and
+#' only shows the output.
 #' 
 #' ```
 #'  ----
 #'  title: "filter-tcrd.tcl documentation"
 #'  author: "Detlef Groth, Caputh-Schwielowsee, Germany"
 #'  date: 2021-12-22
-#'  tdot:
-#'      echo: true
-#'      results: hide
+#'  tcrd:
+#'      echo: 0
 #'      eval: 1
 #'  ----
 #' ```
@@ -82,9 +85,9 @@
 #' 
 #' ```{.tcrd echo=false}
 #' [Intro]
-#'       
+#' 
 #' [G] - [C] - [G] - [C] - [D7] - [G]
-#'       
+#' 
 #' [Verse]
 #' I'[G]ve been a wild rover for many a ye[C]ar
 #' I [G]spent all me m[C]oney on w[D7]hiskey and b[G]eer
@@ -159,6 +162,22 @@
 #' segg wo du heeßt
 #' ```
 #' 
+#' ## Harmomics Tabs
+#' 
+#' You can combine Harmonica Tabs on top with embedded chords, here an example:
+#' 
+#' ```{.tcrd echo=true}
+#' -4  -4  -6    -6 5  -5 5      -4
+#' [Dm]Are you going to [C]Scarborough [Dm]Fair? 
+#' -6  7    -8    7  -6 -7  6   -6
+#' [F]Parsley, [Dm]sage, rose [F]mary [G]and [Dm]thyme 
+#' -8 -8 -8  7  -6 -6  6   -5    5
+#' Remember [F]me to one who [C]lives there 
+#' -4  -6   6  -5  5    -4   4  -4
+#' [Dm]He once [C]was a true love of [Dm]mine
+#' ```
+#'
+#' 
 #' ## Chord diagrams
 #' 
 #' Here an example for a chord diagram for some Ukulele chords, chunk options are `{.tcrd chord=true}`:
@@ -184,6 +203,21 @@
 #' ```
 #' 
 #' To hide the input code you would use `{.tcrd chord=true echo=false}` as a code chunk option.
+#' 
+#' The default output format is svg as this does not require any additional tools. 
+#' If you have `cairosvg` installed you can as well create png or pdf files.
+#' Here an example of a png file. For Guitar chord diagrams you should as well
+#' extend the width of the image by using te width option, here an 
+#' example `{.tcrd chord=true ext=png width=160}`: 
+#' 
+#' ```{.tcrd chord=true ext=png width=160}
+#' C   x32010
+#' Dm  xx0231
+#' Em  022000
+#' F   x03211
+#' G   320003
+#' Am  x02210
+#' ```
 #' 
 #' ## See also:
 #' 
@@ -211,10 +245,12 @@ foreach d $auto_path {
     }
 }
 # TODO
-#tcrdi eval "package require tdot"
 
 namespace eval tcrd {
     proc transpose {note step} {
+        if {![regexp {^[A-G]} $note]} {
+            return $note
+        }
         # check for chord
         set note [string map [list A# Bb C# Db D# Eb F# Gb G# Ab] $note]
         set tp [regsub {^[A-G]b?} $note ""]
@@ -237,7 +273,11 @@ namespace eval tcrd {
         foreach line [split $song "\n"] {
             set chords ""
             set txt    ""
-            if {[regexp {^\s*\[[A-Z][a-z]{3}[ a-z]*\]\s*$} $line]} {
+            if {[regexp {^\s*\[[A-Z0-9][a-z]{3}[ a-z]*\]\s*$} $line]} {
+                append nsong "$line\n"
+                continue
+            }
+            if {[regexp {^\s*[^a-zA-Z]+$} $line]} {
                 append nsong "$line\n"
                 continue
             }
@@ -256,7 +296,7 @@ namespace eval tcrd {
                 continue
             }
             foreach block [split $line " "] {
-                if {[regexp {^\[[A-Za-z0-9]+\].+} $block]} {
+                if {[regexp {^\[[-A-Za-z0-9]+\](.+)} $block]} {
                     # [Chord]text
                     set chord [transpose [regsub {\[(.+)\].+} $block "\\1"] $transpose]
                     set word  [regsub {\[.+\](.+)} $block "\\1"]
@@ -270,18 +310,20 @@ namespace eval tcrd {
                         append chords [string repeat " " [expr {-$diff+1}]]
                         append txt " "
                     }
-                } elseif {[regexp {.+\[[A-Za-z0-9]+\].+} $block]} {
+                } elseif {[regexp {.+\[[-A-Za-z0-9]+\](.+)} $block -> nblock]} {
                     # te[Chord]xt
-                    set chord [transpose [regsub {.+\[(.+)\].+} $block "\\1"] $transpose]
-                    set word1  [regsub {(.+)\[.+\].+} $block "\\1"]
-                    set word2  [regsub {.+\[.+\](.+)} $block "\\1"]                
+                    set chord [transpose [regsub {.+\[(.+?)\].+} $block "\\1"] $transpose]
+                    set word1  [regsub {(.+)\[.+?\].+} $block "\\1"]
+                    set word2  [regsub {.+\[.+?\](.+)} $block "\\1"]                
                     append txt "$word1$word2 "
                     set nchord [string repeat " " [string length $word1]]
                     append nchord $chord
                     set nspaces [expr {[string length $word2] - [string length $chord] + 1}]
                     append nchord [string repeat " " $nspaces]
                     append chords $nchord
-                } elseif {[regexp {^\[[A-Za-z0-9]+\]$} $block]} {
+                    set block $word2
+                    
+                } elseif {[regexp {^\[[-A-Za-z0-9]+\]$} $block]} {
                     # [Chord]
                     set chord [transpose [regsub {\[(.+)\]} $block "\\1"] $transpose]
                     append chords "$chord "
@@ -295,7 +337,11 @@ namespace eval tcrd {
                     append txt "$word "
                 }
             }
-            append nsong "$chords\n$txt\n"
+            if {[regexp {[a-zA-Z]} $txt]} {
+                append nsong "$chords\n$txt\n"
+            } else {
+                append nsong "$chords\n"
+            }
         }
         return $nsong
     }
@@ -343,34 +389,51 @@ namespace eval tcrd {
         }
         return $nsong
     }
-    proc svgchords {name cstring {outfile ""}} {
+    proc svgchords {name cstring args} {
+        array set arg [list -circlecolor maroon -width 100]
+        if {[llength $args] == 1} {
+            set outfile [lindex $args 0]
+        } elseif {[llength $args] > 1}  {
+            array set arg $args
+            if {[info exists arg(-outfile)]} {
+                set outfile $arg(-outfile)
+            } else {
+                set outfile ""
+            }
+        } else {
+            set outfile ""
+        }
         tsvg set code ""
-        tsvg set width 100
+        tsvg set width $arg(-width)
         tsvg set height 255
-        set ystart 50
-        tsvg text x 50 y 30 style "font: bold 24px sans-serif;" text-anchor middle $name 
-        tsvg line x1 10 y1 $ystart x2 92 y2 $ystart stroke-width 5 stroke black
-        set inc [expr {80/([string length $cstring]-1)}]
+        set ystart 48
+        tsvg text x [expr {($arg(-width)/2)}] y 20 style "font: bold 24px sans-serif;" text-anchor middle $name 
+        tsvg line x1 5 y1 $ystart x2 [expr {$arg(-width)-5}] y2 $ystart stroke-width 5 stroke black
+        set inc [expr {($arg(-width)-20)/([string length $cstring]-1)}]
         for { set x 0 } { $x < [string length $cstring] } { incr x } {
-            tsvg line x1 [expr {11+($x*$inc)}] y1 $ystart x2 [expr {10+($x*$inc)}] y2 251 stroke-width 2 stroke black
+            tsvg line x1 [expr {10+($x*$inc)}] y1 $ystart x2 [expr {10+($x*$inc)}] y2 248 stroke-width 2 stroke black
         }
         if {[string length $cstring] > 4} {
-            set r 8
+            set r 9
         } else {
             set r 9
         } 
         set mx 5
         for { set x 0 } { $x < $mx } { incr x } {    
-            tsvg line x1 10 y1 [expr {$ystart+40+($x*40)}] x2 90 y2 [expr {$ystart+40+($x*40)}] stroke-width 2 stroke black
+            tsvg line x1 10 y1 [expr {$ystart+40+($x*40)}] x2 [expr {$arg(-width)-10}] y2 [expr {$ystart+40+($x*40)}] stroke-width 2 stroke black
             for {set y 0} { $y < [string length $cstring] } { incr y } {
                 if {[string range $cstring $y $y] == $x} {
                     if {$x > 0} {
-                        tsvg circle cx [expr {10+1+($y*$inc)}] cy [expr {$ystart-20+($x*40)}] r $r stroke maroon fill maroon
+                        tsvg circle cx [expr {10+1+($y*$inc)}] cy [expr {$ystart-20+($x*40)}] r $r stroke $arg(-circlecolor) fill $arg(-circlecolor)
                     }
                 }
                 if {$x == 0 && [string range $cstring $y $y] == "x"} {
-                    tsvg text x [expr {10+1+($y*$inc)}] y 42 style "font: 20px sans-serif;" text-anchor middle x
+                    tsvg text x [expr {10+1+($y*$inc)}] y 42 style "font: 20px sans-serif;" text-anchor middle X
+                } elseif {$x == 0 && [string range $cstring $y $y] == "0"} {
+                    tsvg text x [expr {10+1+($y*$inc)}] y 42 style "font: 20px sans-serif;" text-anchor middle O
                 }
+
+                
                 
             }
         }
@@ -385,7 +448,8 @@ proc filter-tcrd {cont dict} {
     global n
     incr n
     set def [dict create results show eval true include true label null transpose 0 inline true \
-             chord false chordname "" imagepath images fig false]
+             chord false chordname "" imagepath images fig false ext svg \
+             circlecolor black width 100]
     set dict [dict merge $def $dict]
     if {![dict get $dict eval]} {
         return [list "" ""]
@@ -402,15 +466,17 @@ proc filter-tcrd {cont dict} {
     }
 
     if {[catch {
+         set width [dict get $dict width]
          if {[dict get $dict chord]} {
+             set ext .[dict get $dict ext]
              set svgall ""
              set x 0
              foreach line [split $cont "\n"] {
                  set line [string trim $line]
                  set line [regsub -all { +}  $line " "]
                  foreach {key frets} [split $line " "] {
-                     append svgall "<g transform=\"translate([expr {$x*120}], 0)\">\n"
-                     append svgall [tcrd::svgchords $key $frets]
+                     append svgall "<g transform=\"translate([expr {$x*($width+20)}], 0)\">\n"
+                     append svgall [tcrd::svgchords $key $frets -circlecolor [dict get $dict circlecolor] -width $width]
                      append svgall "\n</g>\n"
                  }
                  incr x
@@ -418,10 +484,22 @@ proc filter-tcrd {cont dict} {
              }
              set out [open $fname w 0600]
              puts $out "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>"
-             puts $out "   <svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" height=\"255\" width=\"[expr {120*$x}]\">"
+             puts $out "   <svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" height=\"255\" width=\"[expr {($width+20)*$x}]\">"
              puts $out $svgall
              puts $out "</svg>"
              close $out
+             if {$ext in [list .pdf .png]} {
+                 if {[auto_execok cairosvg] eq ""} {
+                     error "Conversion to $ext needs the cairosvg tool!"
+                 }
+             } elseif {$ext ne ".svg"}  {
+                 error "Unkown file extension, know file extensions are: .svg, .pdf, .png"
+             }
+             set outfile [regsub {.svg$} $fname $ext]
+             if {$ext in [list .pdf .png]} {
+                 exec cairosvg $fname -o $outfile -W [expr {($width+20)*$x}] -H 255
+                 set fname $outfile
+             } 
              set res $cont
          } elseif {[dict get $dict inline]} {
              set res [tcrd::chords $cont [dict get $dict transpose]] 
